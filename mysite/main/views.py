2c9a3036f46acfd24f399fbf1fc23e9ba75df4b2
cmamani <mamanimezacarlos@gmail.com>
2021-03-18 23:58:07 -0300

    funcionalidad para agregar nuevos items, y poder marcarlos como completos o incompletos

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -10,7 +10,28 @@ def index(response, id):
     ls = ToDoList.objects.get(id=id)
     #item = ls.item_set.get(id=1) #por ahora solo tenemos uno!
     #return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text))) #muestra el id parametro
-    #return render(response, "main/base.html", {"name":ls.name}) 
+    #return render(response, "main/base.html", {"name":ls.name})
+    if response.method == "POST":
+        #save and add items
+        print(response.POST)
+        if response.POST.get("save"):
+            #reviso si los items fueron clickeados
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    #actualizo el item
+                    item.complete = True
+                else:
+                    #si fue desclickeado o no hubo cambios
+                    item.complete = False
+                item.save() #guarda cambios en la base de datos
+            
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid") 
     return render(response, "main/list.html", {"ls":ls})
 
 def home(response):
